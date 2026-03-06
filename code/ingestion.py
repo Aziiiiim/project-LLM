27eb160ccca19c6a10f9c5cde1919b699fe9d524
Fl0r3ntLem <folem0302@gmail.com>
2026-03-06 14:22:46 +0100

    auto ingestion graph

--- code/ingestion.py
+++ code/ingestion.py
@@ -1,43 +1,69 @@
+from typing import Any
+import asyncio
 from neo4j_graphrag.experimental.pipeline.kg_builder import SimpleKGPipeline
 from neo4j import GraphDatabase
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from neo4j_graphrag.llm import OpenAILLM
+from neo4j_graphrag.embeddings import OpenAIEmbeddings
 import json, os
 from dotenv import load_dotenv
 
+import logging
+
+from sqlalchemy import true
+logging.basicConfig(level=logging.DEBUG)
+
 load_dotenv()
 
-with open("schema.json", "r") as f:
-    schema = json.load(f)
+with open("graph_schema.json", "r") as f:
+    schema : dict[str, list[Any]] = json.load(f)
+    if not all(key in schema for key in ["node_types", "relationship_types", "patterns"]):
+        raise ValueError("Schema must contain 'node_types', 'relationship_types' and 'patterns' keys")
 
+print(schema)
 driver = GraphDatabase.driver(
-    os.getenv("NEO4J_URL"), 
-    auth=(os.getenv("NEO4J_USER"), os.getenv("NEO4J_PASSWORD"))
+    os.getenv("NEO4J_URL",""), 
+    auth=(os.getenv("NEO4J_USERNAME",""), os.getenv("NEO4J_PASSWORD",""))
 )
 kg_builder = SimpleKGPipeline(
     driver=driver,
     schema={
         "node_types": schema["node_types"],
         "relationship_types": schema["relationship_types"],
-        "patterns": schema["patterns"],
-        "additional_node_types": False,
+        "patterns": [
+            ("Ingredient", "INGREDIENT_OF", "Recipe"),
+            ("Author", "AUTHOR_OF", "Recipe")
+        ],
+        "additional_node_types": False,         # Only allow labels in NODE_TYPES
+        "additional_relationship_types": False, # Only allow types in REL_TYPES
+        "additional_patterns": False
     },
-    llm=ChatOpenAI(
-        model=os.getenv("AI_MODEL"),
-        base_url=os.getenv("AI_ENDPOINT"),
-        api_key=os.getenv("AI_API_KEY"),
+    llm=OpenAILLM(
+        model_name=os.getenv("AI_MODEL",""),
+        base_url=os.getenv("AI_ENDPOINT",""),
+        api_key=os.getenv("AI_API_KEY",""),
+        model_params={
+            "response_format": {
+                "type": "json_object"
+            }, # Forces adherence
+            "temperature": 0 # Keep it deterministic
+        }
     ),
     embedder=OpenAIEmbeddings(
-        base_url=os.getenv("AI_ENDPOINT"),
-        api_key=os.getenv("AI_API_KEY"),
+        base_url=os.getenv("AI_ENDPOINT",""),
+        api_key=os.getenv("AI_API_KEY",""),
+        model="text-embedding-3-small"
     ),
     from_pdf=False,
-    prompt_template=(
-        "Given the following text, "
-        "extract entities and relationships "
-        "according to the provided schema. ""Return the results in JSON format with 'nodes' and 'relationships' as keys. Text: {text}"),
+    on_error="RAISE",
+    # prompt_template=(
+    #     "Given the following text, "
+    #     "extract entities and relationships "
+    #     "according to the provided schema. ""Return the results in JSON format with 'nodes' and 'relationships' as keys. Text: {text}"),
 )
 
 async def ingest(file_path: str):
     with open(file_path, "r") as f:
         text = f.read()
     await kg_builder.run_async(text=text)
+
+asyncio.run(ingest("recipes/cheesecake.txt"))
