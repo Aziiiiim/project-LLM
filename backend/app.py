854e501333cea810bbb990b5de99a5ba86c39af6
Fl0r3ntLem <folem0302@gmail.com>
2026-03-13 10:35:32 +0100

    WIP main API

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,16 @@
+import os
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from time import sleep
+
+from langchain_openai import ChatOpenAI
+
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger('myapp')
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
@@ -8,9 +18,22 @@ CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
 @app.route('/message', methods=['POST'])
 def receive_message():
     data = request.get_json()
-    print("Received message:", data)
+    logger.info("Received message: %s", data)
     sleep(2)  # Simulate processing time
     return jsonify("This is a response from the backend!")
+
+
+def main(query, model_name:str):
+    model = ChatOpenAI(
+        model=model_name,
+        base_url=os.getenv("AI_ENDPOINT"),
+        api_key=os.getenv("AI_API_KEY"),
+    )
+
+    # model_with_tools = model.bind_tools([calculator, search, get_weather])
+
+    # response = model_with_tools.invoke(query)
+
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=8000)
